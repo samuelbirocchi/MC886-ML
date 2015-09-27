56dddfed1b706365ed7a597cf4caef80bf930144
Samuel Birocchi <samuelbirocchi@gmail.com>
2015-09-27 19:51:25 -0300

    Integração com o Google Maps

--- Trabalho2/Main.R
+++ Trabalho2/Main.R
@@ -1,10 +1,15 @@
 require(nnet)
 require(ggplot2)
+require(ggmap)
+require(hexbin)
 
 data <- read.csv(file = "2015s2-mo444-assignment-02.csv", nrows = 5000)
 
-ggplot(data, aes(x = X, y = Y, colour = Category)) + stat_density2d()
+location <-c(min(data$X), min(data$Y), max(data$X), max(data$Y))
+map <- ggmap(get_map(location = location , scale = "auto", zoom = 11))
 
-model <- glm(Category ~ X + Y, data = data, family = "binomial")
+map + stat_density2d(data = data, aes(x = X, y = Y, colour = Category), contour = TRUE)
 
-model <- multinom(Category ~ X + Y,data = data)
+model <- glm(Category ~ X + Y, data = data, family = "poisson")
+
+model2 <- multinom(Category ~ X + Y,data = data)
